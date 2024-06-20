c55391a74662b74eeea00b41d4f1c7645ed8eefa
Abeer Asidah <abeerasidah@gmail.com>
2024-06-20 16:49:24 +0300

    routes_web

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/cache-clear', function () {
+    Artisan::call('cache:clear');
+    return 'Cache cleared successfully.';
+});
+
+Route::get('/config-clear', function () {
+    Artisan::call('config:clear');
+    return 'Configuration cache cleared successfully.';
+});
+
+Route::get('/route-clear', function () {
+    Artisan::call('route:clear');
+    return 'Route cache cleared successfully.';
+});
+
+Route::get('/view-clear', function () {
+    Artisan::call('view:clear');
+    return 'View cache cleared successfully.';
+});
